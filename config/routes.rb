398355653669b335e6118eb5f982c1ca697a4531
miso160 <hotmisosoup160@gmail.com>
2021-01-03 21:46:10 +0900

    routesの修正とviewをi18n対応に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   root to: 'books#index'
   devise_for :users
   resources :books
-  resources :users, :only => [:index, :show]
+  resources :users, only: [:index, :show]
   
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
