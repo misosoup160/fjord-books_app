1eeae5bcf1b0291c3bdeca2e9a306d01d4b3495b
miso160 <hotmisosoup160@gmail.com>
2021-01-13 21:40:51 +0900

    画像をリサイズとファイル形式の制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,22 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
+  validate :image_type
 
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  private
+
+  def image_type
+    return unless image.attached?
+    if !image.blob.content_type.in?(%('image/jpeg image/jpg image/png image/gif'))
+      errors.add(:image, 'はjpegまたはpngまたはgif形式でアップロードしてください')
     end
   end
 end
