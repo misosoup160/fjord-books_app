142f27e7bb81922f72099a096e09259cab7afdb1
miso160 <hotmisosoup160@gmail.com>
2021-01-20 13:57:53 +0900

    フォローフォロワー一覧の追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,15 @@
 class RelationshipsController < ApplicationController
   def create 
     @user = User.find(params[:follow_id])
-    following = current_user.follow(@user)
-    if following.save
-      flash[:notice] = 'ユーザーをフォローしました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
-    end
+    current_user.follow(@user)
+    flash[:notice] = 'ユーザーをフォローしました'
+    redirect_to @user
   end
 
   def destroy
     @user = User.find(params[:follow_id])
-    following =  current_user.unfollow(@user)
-    if following.destroy
-      flash[:notice] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
-    end
+    current_user.unfollow(@user)
+    flash[:notice] = 'ユーザーのフォローを解除しました'
+    redirect_to @user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followings.order(:id).page(params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.order(:id).page(params[:page])
+    render 'show_follower'
+  end
 end
